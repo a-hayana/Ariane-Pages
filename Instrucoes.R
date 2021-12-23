c8fb656d3d93de88959af253cc90331da6ce3591
Ariane Hayana <economista.ariane@gmail.com>
2021-12-23 13:47:01 -0400

    Initial commit

---
+++ Instrucoes.R
@@ -0,0 +1,9 @@
+devtools::install_github("business-science/portfoliodown")
+
+library(portfoliodown)
+
+new_portfolio_site()
+
+serve_site()
+
+# Ver: https://www.business-science.io/code-tools/2021/12/20/portfoliodown.html?utm_content=buffer99b3c&utm_medium=social&utm_source=linkedin.com&utm_campaign=buffer
